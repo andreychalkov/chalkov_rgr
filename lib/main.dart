23ce35d9d1ad5d5f2e36140acf8a53b4aa2bb523
levmoiseev028 <levmoiseev028@gmail.com>
2024-11-14 19:36:43 +0700

    first

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(git commit -m
       title: 'Работа',
       theme: ThemeData(
         primarySwatch: Colors.blue,
